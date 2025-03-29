f102c601e3d63f606d46548435dc1160235279d7
Mir Mohammad Khaleghi <105988918+mamadkhaleghi@users.noreply.github.com>
2025-03-30 01:28:23 +0330

    Update evaluni_fid.sh

--- script/evaluni_fid.sh
+++ script/evaluni_fid.sh
@@ -9,11 +9,6 @@
 EXPID=$1
 EPOCH=$2
 
+python eval/fid_resize299.py --expid ${EXPID} --epoch ${EPOCH} --eval_type "evaluni"
 
-PROJECT_ROOT="$(pwd)"
-
-GT_PATH="$PROJECT_ROOT/dataset/OPA/com_pic_testpos299"
-EVAL_PATH="$PROJECT_ROOT/result/${EXPID}/eval/${EPOCH}/images299/"
-
-python eval/fid_resize299.py --expid ${EXPID} --epoch ${EPOCH} --eval_type "eval"
-python eval/fid_score.py ${EVAL_PATH} ${GT_PATH} --expid ${EXPID} --epoch ${EPOCH} --eval_type "eval"
+python eval/fid_score.py --expid ${EXPID} --epoch ${EPOCH} --eval_type "evaluni"
